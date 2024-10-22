8f5d8bbdbd8c9eb84342a251d65032441e4ed756
Mohd javed <mohdjaved291@gmail.com>
2024-10-23 05:15:49 +0530

    Final Design

--- script.js
+++ script.js
@@ -167,27 +167,67 @@ function sheryAnimation() {
     })
 }
 
+function flagAnimation() {
+    document.addEventListener("mousemove", (dets) => {
+        gsap.to("#flag", {
+            x: dets.x,
+            y: dets.y
+        });
+    })
+
+    document.querySelector("#hero3").addEventListener("mouseenter", () => {
+        gsap.to("#flag", {
+            opacity: 1
+        });
+    })
+
+    document.querySelector("#hero3").addEventListener("mouseleave", () => {
+        gsap.to("#flag", {
+            opacity: 0
+        });
+    })
+}
+
+function footerAnimation() {
+    let clutter = "";
+    let clutter2 = "";
+
+    document.querySelector("footer h1").textContent.split("").forEach((elem) => {
+        clutter += `<span>${elem}</span>`
+    })
+
+    document.querySelector("footer h2").innerHTML = clutter2;
+
+    document.querySelector("#footer-text").addEventListener("mouseenter", () => {
+        gsap.to("#footer h1 span", {
+            opacity: 0,
+            stagger: 0.05
+        });
+
+        gsap.to("footer h2 span", {
+            delay: 0.35,
+            opacity: 1,
+            stagger: 0.1
+        });
+
+        document.querySelector("#footer-text").addEventListener("mouseleave", () => {
+            gsap.to("footer h1 span", {
+                opacity: 1,
+                stagger: 0.1,
+                delay: 0.35
+            });
+
+            gsap.to("footer h2 span", {
+                opacity: 0,
+                stagger: 0.05
+            });
+        })
+    })
+}
+
 loadingAnimation();
 cursorAnimation();
 locomotiveAnimation();
 sheryAnimation();
-
-
-document.addEventListener("mousemove",(dets)=>{
-    gsap.to("#flag", {
-        x:dets.x,
-        y:dets.y
-    });
-})
-
-document.querySelector("#hero3").addEventListener("mouseenter",()=>{
-    gsap.to("#flag", {
-        opacity:1
-    });
-})
-
-document.querySelector("#hero3").addEventListener("mouseleave", () => {
-    gsap.to("#flag", {
-        opacity: 0 
-    });
-})
+flagAnimation();
+footerAnimation();
